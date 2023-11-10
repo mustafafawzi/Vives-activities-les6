3a72c6877de01df0006e1716e298378abe7fd69e
mustafafawzi <114569862+mustafafawzi@users.noreply.github.com>
2023-11-10 11:15:50 +0100

    Initial commit

---
+++ VivesActivities.Ui.Mvc/Models/VivesActivity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VivesActivities.Ui.Mvc.Models
+{
+    [Table(nameof(VivesActivity))]
+    public class VivesActivity
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+        public required string Location { get; set; }
+    }
+}
